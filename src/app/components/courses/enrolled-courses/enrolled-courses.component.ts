f0ce95ba3d9f01adc0e3d426c3845fbd201d13c2
1taimoorsajjad <taimoorsajjad980@gmail.com>
2023-10-12 17:40:27 +0500

    Added the ngclass in the course section

--- src/app/components/courses/enrolled-courses/enrolled-courses.component.ts
+++ src/app/components/courses/enrolled-courses/enrolled-courses.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./enrolled-courses.component.css']
 })
 export class EnrolledCoursesComponent implements OnInit {
+  isListView = true
+  isGridView = false
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  changeView(view: any) {
+    console.log(view)
+    if (view == 'list') {
+      this.isListView = true;
+      this.isGridView = false;
+    }
+    else if (view == 'grid') {
+      this.isGridView = true;
+      this.isListView = false;
+    }
+
+  }
+
 
 }
